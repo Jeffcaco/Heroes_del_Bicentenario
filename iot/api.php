ff6947fda6fd073b79d6f8b6397f5883a5a76031
johan_mitma12 <avira365@hotmail.com>
2021-09-08 17:16:34 -0500

    add date

--- iot/api.php
+++ iot/api.php
@@ -2,8 +2,12 @@
 $serial=$_REQUEST['serial'];
 $estado=$_REQUEST['estado'];
 
+date_default_timezone_set('America/Lima'); 
+$fecha= date('m-d-Y, h:i:s a', time()); 
+
 echo "<h1>Serial Detectado</h1>";
 echo "<h3>El numero de serial es: ".$serial."</h3><br>";
 echo "<h3>Estado: ".$estado."</h3><br>";
+echo "<h3>Fecha de acceso: ".$fecha."</h3><br>";
 
 ?>
