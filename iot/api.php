a5866991f9d2122e5a673a46851ae9017c22353f
johan_mitma12 <avira365@hotmail.com>
2021-09-08 16:50:51 -0500

    new fixes

--- iot/api.php
+++ iot/api.php
@@ -1,6 +1,8 @@
 <?
 $serial=$_REQUEST['serial'];
 $estado=$_REQUEST['estado'];
+
+echo "<h1>Serial Detectado</h1>";
 echo "<h3>El numero de serial es: ".$serial."</h3><br>";
 echo "<h3>Estado: ".$estado."</h3><br>";
 
