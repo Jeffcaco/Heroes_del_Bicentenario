9a84caf9041299e5baeef732426bcafcd64e9a43
Jeffrey <jeffreycaco_98@hotmail.com>
2021-09-08 00:13:11 -0500

    90%

--- Interfaz_1/inicio/index.php
+++ Interfaz_1/inicio/index.php
@@ -43,15 +43,15 @@
                 <h4 class="text-light  mb-0">Hospital Héroes del Bicentenario<i class="icon ion-md-hand mr-3"></i><i class="icon ion-md-heart mr-3"></i></h4>
             </div>
             <div class="menu">
-                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i>
+                <a href="../../Interfaz_2/dashboard.php" class="d-block text-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i>
                     Dashboard</a>
 
-                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-people lead mr-2"></i>
+                <a href="../../Interfaz_3/personal.php" class="d-block text-light p-3 border-0"><i class="icon ion-md-people lead mr-2"></i>
                     Personal</a>
 
                 <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-stats lead mr-2"></i>
                     Estadísticas</a>
-                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-book lead mr-2"></i></i>
+                <a href="../../Interfaz_3/acceso.php" class="d-block text-light p-3 border-0"><i class="icon ion-md-book lead mr-2"></i></i>
                     Registro de acceso</a>
                 <a href="../cerrar_sesion/cerrar_sesion.php" class="d-block text-light p-3 border-0"><i class="icon ion-md-close lead mr-2"></i></i>
                     Cerrar cesión</a>
@@ -79,7 +79,11 @@
                             <label for="usuario">Usuario : <?php echo $_SESSION['user']; ?> </label> 
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Mi perfil</a>
+                      <a class="dropdown-item" href="../../Interfaz_2/dashboard.php">Dashboard</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="../../Interfaz_3/personal.php">Personal</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="../../Interfaz_3/acceso.php">Registro de acceso</a>
                       <div class="dropdown-divider"></div>
                       <a class="dropdown-item" href="../cerrar_sesion/cerrar_sesion.php">Cerrar sesión</a>
                     </div>
